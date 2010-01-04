5691d1ce0558ac5511f159f64f11d07f87413c76
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-01-04 14:42:10 -0500

    Added Google plugin.

--- irc-bot.rb
+++ irc-bot.rb
@@ -27,6 +27,7 @@ module Mathetes
     end
 
     def initialize_plugins
+      load 'mathetes/plugins/google.rb'
       load 'mathetes/plugins/google-fight.rb'
       load 'mathetes/plugins/memo.rb'
       load 'mathetes/plugins/russian-roulette.rb'
---
+++ lib/mathetes/plugins/google.rb
@@ -0,0 +1,58 @@
+require 'cgi'
+require 'open-uri'
+
+module Mathetes; module Plugins
+
+  class Google
+
+    MAX_RESULTS = 5
+
+    def initialize( mathetes )
+      mathetes.hook_privmsg( :regexp => /^;g(oogle)?\b/ ) do |message|
+        args = message.text[ /^\S+\s+(.*)/, 1 ]
+        nick = message.from.nick
+
+        num_results = 1
+        args_array = args.split( /\s+/ )
+
+        if args_array.length < 1
+          message.answer "!google [number of results] <search terms>"
+          return
+        end
+
+        if args_array[ 0 ].to_i.to_s == args_array[ 0 ]
+          # A number of results has been specified
+          num_results = args_array[ 0 ].to_i
+          if num_results > MAX_RESULTS
+            num_results = MAX_RESULTS
+          end
+          arg = args_array[ 1..-1 ].join( "+" )
+          unescaped_arg = args_array[ 1..-1 ].join( " " )
+        else
+          arg = args_array.join( "+" )
+          unescaped_arg = args_array.join( " " )
+        end
+
+        arg = CGI.escape( arg )
+
+        open( "http://www.google.com/search?q=#{ CGI.escape( args ) }&safe=active" ) do |html|
+          counter = 0
+          html.read.scan /<a href="?([^"]+)" class=l.*?>(.+?)<\/a>/m do |match|
+            url, title = match
+            title.gsub!( /<.+?>/, "" )
+            ua = unescaped_arg.gsub( /-?site:\S+/, '' ).strip
+            message.answer "[#{ua}]: #{url} - #{title}"
+            counter += 1
+            break  if counter >= num_results
+          end
+
+          if counter == 0
+            message.answer "(no results)"
+          end
+        end
+      end
+    end
+
+  end
+
+end; end
