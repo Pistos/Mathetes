2ae01f96774926534df0607104b95d9284a6e272
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-05-05 22:29:53 +0800

    Renamed Op plugin to ChannelUtil.  Added ADMINS Array.  Added !join and !part commands.

--- lib/mathetes/plugins/channel-util.rb
+++ lib/mathetes/plugins/channel-util.rb
@@ -1,15 +1,31 @@
 module Mathetes; module Plugins
 
-  class Op
+  class ChannelUtil
+
+    ADMINS = [
+      'Pistos',
+    ]
 
     def initialize( mathetes )
       @mathetes = mathetes
+
       mathetes.hook_privmsg( :regexp => /^!op\b/ ) do |message|
-        $stderr.puts "hey"
         c = message.channel
         if c
           @mathetes.say "OP #{ c }", 'ChanServ'
         end
+      end
+
+      mathetes.hook_privmsg( :regexp => /^!join (#[A-z0-9_-]+)/ ) do |message|
+        channel = SilverPlatter::IRC::Channel.new( $1 )
+        break  if ! ADMINS.include?( message.from.nick )
+        @mathetes.join_channels( [ channel ] )
+      end
+
+      mathetes.hook_privmsg( :regexp => /^!part (#[A-z0-9_-]+)/ ) do |message|
+        channel = SilverPlatter::IRC::Channel.new( $1 )
+        break  if ! ADMINS.include?( message.from.nick )
+        @mathetes.part_channels( [ channel ] )
       end
     end
 
