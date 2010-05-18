b68c7f4860fccc3acfa4a36aa90b9d4d10e26446
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-05-18 21:59:25 +0800

    Rewrote dictionary plugin because wordsmyth.net changed.

--- lib/mathetes/plugins/dictionary.rb
+++ lib/mathetes/plugins/dictionary.rb
@@ -10,13 +10,12 @@ module Mathetes; module Plugins
         catch :done do
           arg = CGI.escape( message.text[ /^\S+\s+(.*)/, 1 ] )
           doc = Nokogiri::HTML(
-            open( "http://www.wordsmyth.net/live/home.php?script=search&matchent=#{arg}&matchtype=exact" )
+            open( "http://www.wordsmyth.net/?level=3&m=wn&ent=#{arg}" )
           )
 
-
-          not_found_p = doc.at( '[name="p"]' )
-          if not_found_p && not_found_p.content =~ /Sorry, we could not find/
-            suggestions = doc.search( 'a' ).map { |a| a.content }
+          not_found_div = doc.at( 'div.list_title' )
+          if not_found_div && not_found_div.text =~ /Did you mean this word/
+            suggestions = doc.search( 'div.wordlist td a' ).map { |a| a.text }
             output = '(no results)'
             if suggestions.any?
               output << " Close matches: #{suggestions.join( ', ' )}"
@@ -26,72 +25,43 @@ module Mathetes; module Plugins
             throw :done
           end
 
-          tables = doc.search( 'table[cellspacing="0"][border="0"][cellpadding="2"][width="100%"]' )
-          maintable = tables.find { |t| t[ 'align' ].nil? && t[ 'bgcolor' ].nil? }
-          throw :done  if maintable.nil?
-          wordtag = maintable.at( 'div.headword' )
-          if wordtag
-            word = wordtag.children.find_all { |x| x.text? }.map { |t| t.to_s }.join
-          end
-
           output = ""
-          maintable.css( 'tr' ).each do |tr|
 
-            main_td = tr.at( 'td[width="70%"]' )
-            middle_td = tr.at( 'td[width="5%"][valign="baseline"]' )
+          syllabification = doc.at( 'h3.headword.syl' ).text
 
-            # Part of Speech
+          # parts of speech
+          poses = []
+          pos = nil
 
-            if tr[ 'bgcolor' ] == '#DDDDFF'
-              pos = main_td.at( 'span' ).content
-              if ! output.empty?
-                message.answer output
+          trs = doc.search( 'table.maintable tbody tr' )
+          trs.each do |tr|
+            case tr[ 'class' ]
+            when 'postitle'
+              if pos
+                poses << pos
               end
-              output = "#{word} - [#{pos}]"
-            end
-
-            if tr[ 'bgcolor' ] == '#FFFFFF'
-              # Pronunciation
-
-              prontag = tr.search( 'div.pron' )
-              if prontag
-                syllabification = []
-                prontag.css( 'span' ).each do |syllable|
-                  next  if syllable.content.empty?
-
-                  syllable_class = syllable[ 'class' ]
-                  if syllable_class
-                    stress_level = syllable_class[ /(\d)/, 1 ].to_i
-                    case stress_level
-                    when 1
-                      stress = "'"
-                    when 2
-                      stress = '"'
-                    else
-                      stress = ''
-                    end
-                    syllabification << stress + syllable.content
-                  else
-                    syllabification << syllable.content
-                  end
-                end
-                if syllabification.any?
-                  output << " (" + syllabification.join( ' ' ) + ")"
-                end
-              end
-
-              # Definition
-
-              if main_td
-                def_span = main_td.at( 'span[style="font-weight: normal;"]' )
-                if def_span
-                  output << "  " + middle_td.at( 'span' ).content + " " + def_span.content
-                end
-              end
+              pos = {
+                :pos => tr.at( 'td.data a' ).text,
+                :defs => [],
+              }
+            when 'definition'
+              pos[ :defs ] << tr.at( 'td.data' ).children[ 0 ].text
             end
           end
+          if pos
+            poses << pos
+          end
 
-          if ! output.empty?
+          if poses.any?
+            output = syllabification
+            poses.each do |pos|
+              output << "  " << pos[ :pos ] << ": "
+              defs = []
+              pos[ :defs ].each_with_index { |d,i|
+                defs << "#{i+1}. #{d}"
+              }
+              output << defs.join( ' ' )
+            end
             message.answer output
           end
         end
