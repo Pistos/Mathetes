7ff7c54976bc712ba9694745c7116f78d72e7755
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-08-20 00:06:10 -0400

    Put a bit of delay before starting the haiku. Center-justify the whole haiku.

--- lib/mathetes/plugins/haiku.rb
+++ lib/mathetes/plugins/haiku.rb
@@ -10,9 +10,13 @@ module Mathetes; module Plugins
         haikus = Nokogiri::HTML(
           open "http://www.dailyhaiku.org/haiku/?pg=#{ rand(220) + 1 }"
         ).search( 'p.haiku' ).to_a
-        haikus[ rand( haikus.size ) ].text.split( /[\r\n]+/ ).each do |line|
-          message.answer line
+        haiku_lines = haikus[ rand( haikus.size ) ].text.split( /[\r\n]+/ )
+        width = haiku_lines.inject(0) { |max,line|
+          line.length > max ? line.length : max
+        }
+        haiku_lines.each do |line|
           sleep 3
+          message.answer line.center( width )
         end
       end
     end
