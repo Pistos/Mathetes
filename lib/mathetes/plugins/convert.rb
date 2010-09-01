3e36f64979fd8d14b1642461592005d8dd201c1b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-09-02 03:14:16 +0800

    Updated Google parsing for conversion/calculation plugin.  Also unescape entities.

--- lib/mathetes/plugins/convert.rb
+++ lib/mathetes/plugins/convert.rb
@@ -17,8 +17,8 @@ module Mathetes; module Plugins
         arg = CGI.escape( message.text[ /^\S+\s+(.*)/, 1 ] )
         open( "http://www.google.com/search?q=#{ arg }" ) do |html|
           answered = false
-          html.read.scan /calc_img.+?<b>(.+?)<\/b>/ do |result|
-            stripped_result = result[ 0 ]
+          html.read.scan %r{<h2.*style="font-size:138%"><b>(.+?)</b></h2>}m do |result|
+            stripped_result = CGI.unescapeHTML( result[ 0 ] )
             stripped_result = stripped_result.gsub( /<sup>(.+?)<\/sup>/, "^(\\1)" )
             stripped_result = stripped_result.gsub( /<font size=-2> <\/font>/, "" )
             stripped_result = stripped_result.gsub( /<[^>]+>/, "" )
