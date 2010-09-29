3c72049841bdd9f85dbda039c2071a5b5252d67b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-09-30 03:13:03 +0800

    Use new ZeptoURL host.

--- lib/mathetes/plugins/github-hook.rb
+++ lib/mathetes/plugins/github-hook.rb
@@ -74,7 +74,7 @@ module Mathetes; module Plugins
     end
 
     def zepto_url( url )
-      URI.parse( 'http://zep.purepistos.net/zep/1?uri=' + CGI.escape( url ) ).read
+      URI.parse( 'http://z.pist0s.ca/zep/1?uri=' + CGI.escape( url ) ).read
     rescue
       # (return nil)
     end
