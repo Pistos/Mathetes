89b89d3b696b65f1cc817efc8ee0a393e9143b4a
Pistos <gitsomegrace.5.pistos@geoshell.com>
2011-02-27 22:24:11 -0500

    Added www prefix to downforeveryoneorjustme.com, which gives a 300-series redirect.

--- lib/mathetes/plugins/down-for-me.rb
+++ lib/mathetes/plugins/down-for-me.rb
@@ -10,7 +10,7 @@ module Mathetes; module Plugins
       mathetes.hook_privmsg( :regexp => /^(!(up|down)|(up|down)\?)\b/ ) do |message|
         terms = message.text[ /^\S+\s+(.*)/, 1 ]
         site = terms.downcase[ /([a-z0-9.-]+)($|\/)/, 1 ]
-        doc = Nokogiri::HTML( open( "http://downforeveryoneorjustme.com/#{site}" ) )
+        doc = Nokogiri::HTML( open( "http://www.downforeveryoneorjustme.com/#{site}" ) )
         message.answer "#{message.from.nick}: [#{site}] " + doc.at( 'div#container' ).children.select{ |e| e.text? }.join( ' ' ).gsub( /\s+/, ' ' ).strip
       end
     end
