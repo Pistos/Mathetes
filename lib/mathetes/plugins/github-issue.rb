c646c9687c9093e63ed27196520b7b2581234a40
Sam Gleske <sam.mxracer@gmail.com>
2012-03-05 23:38:24 -0500

    Took the generic sample stuff out of the github-issue plugin.

--- lib/mathetes/plugins/github-issue.rb
+++ lib/mathetes/plugins/github-issue.rb
@@ -1,17 +1,12 @@
 module Mathetes; module Plugins
-  class Sample
+  class GitHubIssue
     def initialize( mathetes )
       mathetes.hook_privmsg(
         :regexp => /#[0-9]+/
       ) do |message|
         issues = message.text[ /#([0-9]+)/, 1 ]
 	issues.each { |issue| message.answer "https://github.com/openphoto/frontend/issues/#{issue}" }
-        #handle_privmsg message
       end
-    end
-
-    def handle_privmsg( message )
-      message.answer "Foo to you!"
     end
   end
 end; end
